e4b888d412f5632497e9216c7f6fe581b75291b2
Tega McKinney <tega.mckinney@flexdrive.com>
2019-11-15 03:20:32 -0500

    add kops supporting module

---
+++ aws/f1-platform/kops-aws-platform/external-dns.tf
@@ -0,0 +1,36 @@
+module "kops_external_dns" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-kops-external-dns.git?ref=tags/0.2.2"
+  namespace      = "${var.namespace}"
+  stage          = "${var.stage}"
+  name           = "external-dns"
+  cluster_name   = "${coalesce(var.cluster_name_prefix, var.region)}.${var.zone_name}"
+  dns_zone_names = "${var.dns_zone_names}"
+
+  tags = {
+    Cluster = "${coalesce(var.cluster_name_prefix, var.region)}.${var.zone_name}"
+  }
+}
+
+output "kops_external_dns_role_name" {
+  value = "${module.kops_external_dns.role_name}"
+}
+
+output "kops_external_dns_role_unique_id" {
+  value = "${module.kops_external_dns.role_unique_id}"
+}
+
+output "kops_external_dns_role_arn" {
+  value = "${module.kops_external_dns.role_arn}"
+}
+
+output "kops_external_dns_policy_name" {
+  value = "${module.kops_external_dns.policy_name}"
+}
+
+output "kops_external_dns_policy_id" {
+  value = "${module.kops_external_dns.policy_id}"
+}
+
+output "kops_external_dns_policy_arn" {
+  value = "${module.kops_external_dns.policy_arn}"
+}
