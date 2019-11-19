13449a0c80eb69150326a82a5b2d7cc8216d5d44
Tega McKinney <tega.mckinney@flexdrive.com>
2019-11-19 16:55:54 -0500

    Correct field name. Rename some fields

--- aws/f1-platform/backing-services/mongodb-atlas.tf
+++ aws/f1-platform/backing-services/mongodb-atlas.tf
@@ -59,13 +59,13 @@ locals {
   atlas_project_name = "${var.namespace}-${var.stage}-${var.atlas_project_name}"
 }
 
-resource "mongodbatlas_project" "my_project" {
+resource "mongodbatlas_project" "f1_project" {
   name   = "${local.atlas_project_name}"
   org_id = "${data.aws_ssm_parameter.mongodbatlas_org_id.value}"
 }
 
-resource "mongodbatlas_network_container" "network_container" {
-  project = "${mongodbatlas_project.my_project.project_id}"
+resource "mongodbatlas_network_container" "f1_network" {
+  project_id = "${mongodbatlas_project.f1_project.project_id}"
   atlas_cidr_block = "${var.atlas_cidr_block}"
   provider_name = "AWS"
   region_name = "${var.region}"
@@ -73,15 +73,15 @@ resource "mongodbatlas_network_container" "network_container" {
 
 resource "mongodbatlas_network_peering" "peering" {
   accepter_region_name    = "${var.region}"   
-  project_id              = "${mongodbatlas_network_container.network_container.project_id}"
-  container_id            = "${mongodbatlas_network_container.network_container.container_id}"
+  project_id              = "${mongodbatlas_network_container.f1_network.project_id}"
+  container_id            = "${mongodbatlas_network_container.f1_network.container_id}"
   provider_name           = "AWS"
   route_table_cidr_block  = "${data.aws_ssm_parameter.kops_network_cidr.value}"
   vpc_id                  = "${module.kops_metadata.vpc_id}"
   aws_account_id          = "${var.aws_account_id}"
 }
 
-resource "mongodbatlas_cluster" "atlas_cluster" {
+resource "mongodbatlas_cluster" "f1_atlas_cluster" {
   project_id   = "${mongodbatlas_network_container.network_container.project_id}"
   name         = "${local.atlas_cluster_name}"
   num_shards   = 1
