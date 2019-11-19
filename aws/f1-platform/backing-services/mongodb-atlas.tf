c9cb2a7c4defdd2fa962c07ad03e311982ce6466
Tega McKinney <tega.mckinney@flexdrive.com>
2019-11-19 16:48:18 -0500

    Enable mongo atlas cluster creation

--- aws/f1-platform/backing-services/mongodb-atlas.tf
+++ aws/f1-platform/backing-services/mongodb-atlas.tf
@@ -10,17 +10,96 @@ data "aws_ssm_parameter" "mongodbatlas_org_id" {
   name = "/mongodbatlas/org_id"
 }
 
+data "aws_ssm_parameter" "kops_network_cidr" {
+  name = "/kops/${var.stage}/${var.cluster_name_prefix}/kops_network_cidr"
+}
+
 provider "mongodbatlas" {
   public_key  = "${data.aws_ssm_parameter.mongodbatlas_public_key.value}"
   private_key = "${data.aws_ssm_parameter.mongodbatlas_private_key.value}"
 }
 
-variable "mongodbatlas_project_name" {
+variable "atlas_project_name" {
   type        = "string"
   description = "Name of the Mongo Atlas Project"
 }
 
+variable "atlas_cidr_block" {
+  type = "string"
+  default = ""
+  description = "The CIDR block for the Atlas network"
+}
+
+variable "aws_account_id" {
+  type = "string"
+}
+variable "atlas_cluster_name" {
+  type = "string"
+}
+variable "atlas_disk_size_gb" {
+  type = "string"
+  default = "10"
+}
+variable "atlas_provider_disk_iops" {
+  type = "string"
+  default = "100"
+}
+variable "atlas_provider_volume_type" {
+  type = "string"
+  default = "STANDARD"
+}
+variable "atlas_provider_encrypt_ebs_volume" {
+  type = "string"
+  default = "true"
+}
+variable "atlas_instance_size" {
+  type = "string"
+  default = "M10"
+}
+
+locals {
+  atlas_cluster_name = "${var.stage}-${var.atlas_project_name}"
+  atlas_project_name = "${var.namespace}-${var.stage}-${var.atlas_project_name}"
+}
+
 resource "mongodbatlas_project" "my_project" {
-  name   = "${var.mongodbatlas_project_name}"
+  name   = "${local.atlas_project_name}"
   org_id = "${data.aws_ssm_parameter.mongodbatlas_org_id.value}"
+}
+
+resource "mongodbatlas_network_container" "network_container" {
+  project = "${mongodbatlas_project.my_project.project_id}"
+  atlas_cidr_block = "${var.atlas_cidr_block}"
+  provider_name = "AWS"
+  region_name = "${var.region}"
+}
+
+resource "mongodbatlas_network_peering" "peering" {
+  accepter_region_name    = "${var.region}"   
+  project_id              = "${mongodbatlas_network_container.network_container.project_id}"
+  container_id            = "${mongodbatlas_network_container.network_container.container_id}"
+  provider_name           = "AWS"
+  route_table_cidr_block  = "${data.aws_ssm_parameter.kops_network_cidr.value}"
+  vpc_id                  = "${module.kops_metadata.vpc_id}"
+  aws_account_id          = "${var.aws_account_id}"
+}
+
+resource "mongodbatlas_cluster" "atlas_cluster" {
+  project_id   = "${mongodbatlas_network_container.network_container.project_id}"
+  name         = "${local.atlas_cluster_name}"
+  num_shards   = 1
+
+  replication_factor           = 3
+  backup_enabled               = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "4.0"
+
+  //Provider Settings "block"
+  provider_name               = "AWS"
+  disk_size_gb                = "${var.atlas_disk_size_gb}"
+  provider_disk_iops          = "${var.atlas_provider_disk_iops}"
+  provider_volume_type        = "${var.atlas_provider_volume_type}"
+  provider_encrypt_ebs_volume = "${var.atlas_provider_encrypt_ebs_volume}"
+  provider_instance_size_name = "${var.atlas_instance_size}"
+  provider_region_name        = "${var.region}"
 }
