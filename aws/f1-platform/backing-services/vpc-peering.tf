a6b7067386b433cb1d4d4ae77d597f6a451bd53c
Tega McKinney <tega.mckinney@flexdrive.com>
2019-12-05 10:57:03 -0500

    Add ability to disable peering

--- aws/f1-platform/backing-services/vpc-peering.tf
+++ aws/f1-platform/backing-services/vpc-peering.tf
@@ -1,5 +1,11 @@
+variable "vpc_peering_enabled" {
+  type = "string"
+  default = "false"
+}
+
 module "vpc_peering" {
   source           = "git::https://github.com/cloudposse/terraform-aws-vpc-peering.git?ref=0.11/master"
+  enabled          = "${var.vpc_peering_enabled}"
   namespace        = "${var.namespace}"
   stage            = "${var.stage}"
   name             = "${local.name}"
